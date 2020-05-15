f188c624e6f471fd75278c5ffdb046af1970abf3
José Alexandre Ramos Neto <65056403+alexandre-ramos94@users.noreply.github.com>
2020-05-15 18:49:30 -0300

    tratamento de erros
    Foi adicionado um tratamento de erros para que o programa aceite numeros de cpf digitador com " . " e "  - " como normalmente sao formatados e exibidos.

--- cpf.py
+++ cpf.py
@@ -1,30 +1,35 @@
 from random import randint
 from tkinter import *
 
+
 def verifica():
+    """Esta função irá verificar a validade de um número de cpf baseado em principios matemáticos"""
     global lb
-    lb['text'] = ''
-    tot = x = 0
-    teste = list()
-    cp = str(ed.get())
-    nb = cp[9]
-    for c in cp:
-        teste.append(int(c))
-    for k in range(10, 1, -1):
-        x += k * int(teste[tot])
-        tot += 1
-    if x % 11 == 0 or x % 11 == 1 and nb == 0 or int(nb) == (11 - (x % 11)):
-       tot = x = 0
-       nb2 = cp[10]
-       for c in range(11, 1, -1):
-           x += c * teste[tot]
-           tot += 1
-       if x % 11 == 0 or x % 11 == 1 and nb2 == 0 or int(nb2) == (11 - (x % 11)):
-           lb['text'] = 'CPF válido'
-       else:
-           lb['text'] = 'CPF inválido'
-    else:
-        lb['text'] = 'CPF inválido'
+    try:
+        lb['text'] = ''
+        tot = x = 0
+        teste = list()
+        cp = str(ed.get().replace('.', '').replace('-', ''))
+        nb = cp[9]
+        for c in cp:
+            teste.append(int(c))
+        for k in range(10, 1, -1):
+            x += k * int(teste[tot])
+            tot += 1
+        if x % 11 == 0 or x % 11 == 1 and nb == 0 or int(nb) == (11 - (x % 11)):
+           tot = x = 0
+           nb2 = cp[10]
+           for c in range(11, 1, -1):
+               x += c * teste[tot]
+               tot += 1
+           if x % 11 == 0 or x % 11 == 1 and nb2 == 0 or int(nb2) == (11 - (x % 11)):
+               lb['text'] = 'CPF válido'
+           else:
+               lb['text'] = 'CPF inválido'
+        else:
+            lb['text'] = 'CPF inválido'
+    except:
+        lb['text'] = 'Houve algum problema com os dados, verifique-os e tente novamente'
 
 
 def gerador():
@@ -83,10 +88,11 @@ lb = Label(janela, text='')
 lb.pack()
 bt = Button(janela, width=20, text='Gerar cpf', command=gerador)
 bt.pack()
-ed = Entry(janela, width="20")
-ed.pack()
-bt2 = Button(janela, width=20, text="teste", command=verifica)
+bt2 = Button(janela, width=20, text="Verificar nº de cpf", command=verifica)
 bt2.pack()
+ed = Entry(janela, width=23)
+ed.pack()
+
 janela.mainloop()
 
 
